2c83785878e59f592f102962cb93cd6f55dae872
hindsal <hindsal@umich.edu>
2025-07-19 02:00:14 -0400

    Design dashboard tiles for multi-project view functionality

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <Navigation />
     <Slider />
+    <DashboardTiles />
   </div>
 </template>
 
 <script>
 import Navigation from './components/Navigation.vue'
 import Slider from './components/Slider.vue'
+import DashboardTiles from './components/DashboardTiles.vue'
 
 export default {
   components: {
     Navigation,
-    Slider
+    Slider,
+    DashboardTiles
   }
 }
 </script>
